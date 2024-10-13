8369f144f608ee798ffa2e61922c3a5e533e1da1
drong24 <daisyrong24@gmail.com>
2024-10-12 21:09:37 -0700

    created a seperate function for adding fade in animations

--- script.js
+++ script.js
@@ -3,24 +3,27 @@
 // adds fade in animation for about section when site is loaded
 window.addEventListener('load', () => {
     const aboutSection = document.getElementById('about');
-    aboutSection.style.opacity = '1';
-    aboutSection.style.transform = 'translateY(0)';
+    addFadeInAnimation(aboutSection);
 })
 // adds fade in animation when a section enters the viewport
 window.addEventListener('scroll', function() {
     const sections = document.querySelectorAll('.fade_in'); // target all "fade_in" sections
     sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top; // get the position of the section relative to the viewport
-        const sectionBottom = section.getBoundingClientRect().bottom; // get the position of the section's bottom relative to the viewport
-        const windowHeight = window.innerHeight;
-
-        // check if ANY part of the section is in the viewport
-        if (sectionTop < windowHeight && sectionBottom > 0) {
-            section.style.opacity = '1';
-            section.style.transform = 'translateY(0)';
-        } else {
-            section.style.opacity = '0';
-            section.style.transform = 'translateY(50px)';
-        }
+        addFadeInAnimation(section);
     });
 });
+
+// used by scroll and load event listeners to add fade in animation
+function addFadeInAnimation(element) {
+    const elementTop = element.getBoundingClientRect().top;
+    const elementBottom = element.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+    
+    if (elementTop < windowHeight && elementBottom > 0) {
+        element.style.opacity = '1';
+        element.style.transform = 'translateY(0)';
+    } else {
+        element.style.opacity = '0';
+        element.style.transform = 'translateY(50px)';
+    }
+}
